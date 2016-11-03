a70418fba0be493d118c2426a5bd47c50a7a63be
Aron <abeal@eelzee.com>
2016-11-03 14:40:34 -0700

    (master) - Formalizes the changes to the Cache interface, and cleans up both it and CacheBase

--- src/Drupal/DrupalExtension/Context/Cache/CacheBase.php
+++ src/Drupal/DrupalExtension/Context/Cache/CacheBase.php
@@ -11,7 +11,7 @@ use Drupal\DrupalExtension\Context\RawDrupalContext as Context;
  *  with indexing.  Note: not all interface methods are implemented!  It
  *  is up to the subclass to fill in the blanks.
  */
-abstract class CacheBase implements CacheInterface {
+abstract class CacheBase implements CacheInterface, CacheInterface {
   // Stores actual copies of cached items.  Using stdclass to allow
   // "string" integer keys.
   protected $cache = NULL;
@@ -37,60 +37,32 @@ abstract class CacheBase implements CacheInterface {
   }
 
   /**
-   * Resets cache storage.
+   * Magic method to display cache contents as a CLI-formatted string.
    *
-   * Should only be called internally by the clean method, as that method does
-   * db cleanup as a side-effect before calling, which would otherwise not
-   * be accomplished.
+   * @return string
+   *   A cli-formatted string describing the state of the cache, showing
+   *   a list of current keys and indices (but not values, which would
+   *   usually be overly verbose.
    */
-  protected function resetCache() {
-
-    $this->cache = new \stdClass();
-    // $this->hash = new \stdClass();
-    foreach ($this->getNamedIndices() as $k) {
-      // Print "Creating named index: $k\n";.
-      $this->indices->{$k} = new \stdClass();
-    }
-  }
-
-  /**
-   * {@inheritDoc}.
-   */
-  public function getNamedIndices() {
-
-    return array_keys(get_object_vars($this->indices));
-  }
-
-  /**
-   * Provides a list of the keys assigned to objects in this cache.
-   *
-   * @return array
-   *   An array of string keys.
-   */
-  protected function getCacheIndicies() {
-
-    return array_keys(get_object_vars($this->cache));
-  }
-
-  /**
-   * {@InheritDoc}.
-   */
-  public function addIndices() {
-
-    $named_indices = func_get_args();
-    if (empty($named_indices)) {
-      throw new \Exception(sprintf("%s:: No arguments passed to %s function", get_class($this), __FUNCTION__));
-    }
-    foreach ($named_indices as $named_index) {
-      if (!property_exists($this->indices, $named_index)) {
-        $this->indices->{$named_index} = new \stdClass();
+  public function __toString() {
+    $index_values = array();
+    $result = "\n**************************";
+    $result .= "\n " . get_class($this);
+    $result .= "\n**************************\nCache entry count: " . $this->count();
+    $result .= "\nKeys: " . implode(', ', $this->getCacheIndicies());
+    $result .= "\nIndices: ";
+    foreach ($this->getNamedIndices() as $index_name) {
+      $result .= "\nIndex values stored in $index_name";
+      foreach ($this->indices->{$index_name} as $index_value) {
+        $result .= "\n\t$index_value";
       }
     }
-
+    $result .= "\n**************************\n";
+    return $result;
   }
 
   /**
-   * {@InheritDoc}.
+   * {@inheritdoc}
    */
   public function add($index, $value = NULL) {
     if (empty($index)) {
@@ -128,7 +100,6 @@ abstract class CacheBase implements CacheInterface {
         // Can't perform an index lookup on a non-scalar value.
         continue;
       }
-      // Print sprintf("%s::%s line %s: Adding %s->%s index with value %s\n", get_class($this), __FUNCTION__, __LINE__, $index_name, $index_value, $index);.
       if (!isset($this->indices->{$index_name}->{$index_value})) {
         $this->indices->{$index_name}->{$index_value} = array();
       }
@@ -138,30 +109,24 @@ abstract class CacheBase implements CacheInterface {
   }
 
   /**
-   * Finds an item in the cache that matches the passed values.
-   *
-   * Default behavior does not implement, and throws exception if
-   * invoked in a subclass.  Override as necessary.
-   *
-   * @param array $values
-   *   An array where the keys are property names on the cached object, and
-   *   where values are the property values.
+   * {@inheritdoc}
    */
-  public function find(array $values, Context &$context) {
+  public function addIndices() {
 
-    throw new \Exception(sprintf("%s: does not implement the %s method", get_class($this), __FUNCTION__));
+    $named_indices = func_get_args();
+    if (empty($named_indices)) {
+      throw new \Exception(sprintf("%s:: No arguments passed to %s function", get_class($this), __FUNCTION__));
+    }
+    foreach ($named_indices as $named_index) {
+      if (!property_exists($this->indices, $named_index)) {
+        $this->indices->{$named_index} = new \stdClass();
+      }
+    }
+
   }
 
   /**
-   * {@InheritDoc}.
-   */
-  public function count() {
-
-    return count(array_keys(get_object_vars($this->cache)));
-  }
-
-  /**
-   * {@InheritDoc}.
+   * {@inheritdoc}
    */
   public function clean(Context &$context) {
 
@@ -173,34 +138,59 @@ abstract class CacheBase implements CacheInterface {
   }
 
   /**
-   * {@InheritDoc}.
+   * {@inheritdoc}
    */
-  public function remove($key, Context &$context) {
+  public function count() {
 
-    throw new \Exception(sprintf("%s:: does not implement the %s method %", get_class($this), __FUNCTION__));
+    return count(array_keys(get_object_vars($this->cache)));
   }
 
   /**
-   * Returns the item found at the named index.
-   *
-   * An index is different than an alias - indices are defined upon cache
-   * creation, and are populated automatically when new items are added.  For
-   * example, a user index might be 'name', in which case the user cache would
-   * need to capture the value of 'name' and store it referencing the user id.
-   *
-   * Example usage: `getIndex('name', 'Fred')`
-   *
-   * @param string $index_name
-   *   A known named index items in this cache are stored under.
-   * @param string $index_key
-   *   The value of the index key to search.
+   * {@inheritdoc}
+   */
+  public function deleteValue($key, $field, Context &$context) {
+    throw new \Exception(sprintf("%s::%s line %s: No implementation available.", get_class($this), __FUNCTION__, __LINE__));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function find(array $values, Context &$context) {
+
+    throw new \Exception(sprintf("%s: does not implement the %s method", get_class($this), __FUNCTION__));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function get($key, Context &$context) {
+
+    if (!property_exists($this->cache, $key)) {
+      throw new \Exception(sprintf("%s::%s: No result found for key %s.", get_class($this), __FUNCTION__, $key));
+    }
+    return $this->cache->{$key};
+  }
+
+  /**
+   * Provides a list of the keys assigned to objects in this cache.
    *
    * @return array
-   *   An array of items stored at the index, or NULL if there was no entry
-   *   with value $k within the given index.
-   *
-   * @throws \Exception
-   *   If the named index is not valid (which should be known before runtime).
+   *   An array of string keys.
+   */
+  protected function getCacheIndicies() {
+
+    return array_keys(get_object_vars($this->cache));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEntityType() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public function getIndex($index_name, $index_key) {
 
@@ -214,18 +204,15 @@ abstract class CacheBase implements CacheInterface {
   }
 
   /**
-   * {@InheritDoc}.
+   * {@inheritdoc}
    */
-  public function get($key, Context &$context) {
+  public function getNamedIndices() {
 
-    if (!property_exists($this->cache, $key)) {
-      throw new \Exception(sprintf("%s::%s: No result found for key %s.", get_class($this), __FUNCTION__, $key));
-    }
-    return $this->cache->{$key};
+    return array_keys(get_object_vars($this->indices));
   }
 
   /**
-   * Returns the value for a field from a given alias.
+   * {@inheritdoc}
    */
   public function getValue($key, $field, Context &$context) {
     $object = $this->get($key, $context);
@@ -237,44 +224,28 @@ abstract class CacheBase implements CacheInterface {
   }
 
   /**
-   * Deletes the field value for a given alias.
+   * {@inheritdoc}
    */
-  public function deleteValue($key, $field, Context &$context) {
-    throw new \Exception(sprintf("%s::%s line %s: No implementation available.", get_class($this), __FUNCTION__, __LINE__));
+  public function remove($key, Context &$context) {
+
+    throw new \Exception(sprintf("%s:: does not implement the %s method %", get_class($this), __FUNCTION__));
   }
 
   /**
-   * @return string
-   *   The entity type stored by this cache, or FALSE if the cache stores
-   *   something other than entities.
-   */
-  public function getEntityType() {
-    return FALSE;
-  }
-
-  /**
-   * Magic method to display cache contents as a CLI-formatted string.
+   * Resets cache storage.
    *
-   * @return string
-   *   A cli-formatted string describing the state of the cache, showing
-   *   a list of current keys and indices (but not values, which would
-   *   usually be overly verbose.
+   * Should only be called internally by the clean method, as that method does
+   * db cleanup as a side-effect before calling, which would otherwise not
+   * be accomplished.
    */
-  public function __toString() {
-    $index_values = array();
-    $result = "\n**************************";
-    $result .= "\n " . get_class($this);
-    $result .= "\n**************************\nCache entry count: " . $this->count();
-    $result .= "\nKeys: " . implode(', ', $this->getCacheIndicies());
-    $result .= "\nIndices: ";
-    foreach ($this->getNamedIndices() as $index_name) {
-      $result .= "\nIndex values stored in $index_name";
-      foreach ($this->indices->{$index_name} as $index_value) {
-        $result .= "\n\t$index_value";
-      }
+  protected function resetCache() {
+
+    $this->cache = new \stdClass();
+    // $this->hash = new \stdClass();
+    foreach ($this->getNamedIndices() as $k) {
+      // Print "Creating named index: $k\n";.
+      $this->indices->{$k} = new \stdClass();
     }
-    $result .= "\n**************************\n";
-    return $result;
   }
 
 }
